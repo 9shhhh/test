bce19d166d37c95d11ba91581170204dd80af784
sinhookim <shkim@yongsanzip.com>
2019-06-18 21:15:51 -0700

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            hasResult: function () {
+                return this.posts.length > 0
+            }
+        },
+        mounted() {
+            console.log('ddd');
+            this.$http.get('/user')
+                .then( response => { console.log(response); } ) // SUCCESS
+                .catch( response => { console.log(response); } ); // ERROR
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const { mix } = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'js')
+    .sass('resources/sass/app.scss', 'css')
+    .webpackConfig({
+        output: {
+            publicPath: 'http://localhost:8000/'
+        }
+    })
